a7002b65eecb76f55b593738acf4a7328b276524
Guilherme Sobreira da Silva <111311934+GuilhermeSobreira@users.noreply.github.com>
2024-11-08 09:04:54 -0300

    Add files via upload

---
+++ Arvore/src/arvore/Principal.java
@@ -0,0 +1,35 @@
+package arvore;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		No n = new No();
+		Estrutura e = new Estrutura();
+		e.inserir(7);
+		e.inserir(5);
+		e.inserir(3);
+		e.inserir(6);
+		e.inserir(4);
+		e.inserir(2);
+		e.inserir(1);
+		e.inserir(9);
+		e.inserir(8);
+		
+		e.preOrdem();
+		System.out.println("\n________________________________\n");
+		e.posOrdem();
+		System.out.println("\n________________________________\n");
+		e.inOrdem();
+		
+		n = e.valorMinimo();
+		System.out.println("\nValor minimo = " + n.getValor());
+		
+		n = e.valorMaximo();
+		System.out.println("\nValor máximo = " + n.getValor());
+		
+		n = e.removerMenor();
+		System.out.println("\nRemover menor = " + e.removerMenor());
+	}
+
+}
